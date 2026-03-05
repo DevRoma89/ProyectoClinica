171b4abd0079a58c3dd2d5a71e6ead61f5f90f90
Stormname <Florentinmonica34@gmail.com>
2026-03-05 18:44:08 -0300

    feat(entidades) Receta

---
+++ Server/ProyectoClinica.Shared/Entidades/Receta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoClinica.Shared.Entidades
+{
+    public class Receta
+    {
+
+        public int Id { get; set; } 
+
+
+    }
+}
