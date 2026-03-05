01a2c6bcd7d24cfb8f836745e64bb753330628df
Julio6545 <juliodelvallebritez@gmail.com>
2026-03-05 12:55:05 -0300

    Agregar Turnos y DisponibilidadMedico

--- Server/ProyectoClinica.Server/AppDbContext.cs
+++ Server/ProyectoClinica.Server/AppDbContext.cs
@@ -19,6 +19,8 @@ namespace ProyectoClinica.Server
         public DbSet<HistoriaClinica> HistoriaClinicas { get; set;}
         public DbSet<AntecedenteMedico> AntecedenteMedicos { get; set;}
 
+        public DbSet<DisponibilidadMedico> disponibilidadMedicos { get; set; }
+        public DbSet<GestionTurno> GestionTurnos { get; set; }
 
     }
 }
---
+++ Server/ProyectoClinica.Shared/Entidades/DisponibilidadMedico.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoClinica.Shared.Entidades
+{
+    public class DisponibilidadMedico
+    {
+       
+        public int Id { get; set; }
+
+    
+        public DayOfWeek DiaSemana { get; set; }
+
+        public TimeSpan HoraInicio { get; set; }
+
+       
+        public TimeSpan HoraFin { get; set; }
+
+        public int DuracionTurnoMinutos { get; set; }
+
+
+        public Medico Medico { get; set; }
+    }
+}
