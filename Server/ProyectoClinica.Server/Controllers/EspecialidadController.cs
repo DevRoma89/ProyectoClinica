cf872dc60bceb5e8c3504e4e6af066705efe096f
Augusto Ojeda Notebook <manuojedauniversidad@gmail.com>
2026-03-04 21:06:50 -0300

    Creacion de Controllador para las Especialidades

---
+++ Server/ProyectoClinica.Server/Controllers/EspecialidadController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoClinica.Shared.Entidades;
+
+namespace ProyectoClinica.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EspecialidadController:ControllerBase
+    {
+
+        private readonly AppDbContext context;
+
+        public EspecialidadController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Especialidad>>> Get()
+        {
+            return await context.Especialidades.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Especialidad especialidad)
+        {
+
+            if (String.IsNullOrEmpty(especialidad.Nombre))
+            {
+                return BadRequest("No puede agregar una especialidad con nombre vacio");
+            }
+            if (String.IsNullOrEmpty(especialidad.Descripcion))
+            {
+                return BadRequest("No puede agregar una especialidad con descripcion vacia");
+            }
+
+            context.Add(especialidad);
+
+            await context.SaveChangesAsync();
+
+            return Ok(especialidad);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Delete()
+        {
+            return Ok();
+        }
+    }
+}
