fb517dae0fcbe901f00e5be2e595a59afd519cda
3lRoman <eliasroman2003@gmail.com>
2026-03-04 23:13:20 -0300

    feat(historia-clinica): agregar entidades HistoriaClinica y AntecedenteMedico

---
+++ Server/ProyectoClinica.Shared/Entidades/HistoriaClinica.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoClinica.Shared.Entidades
+{
+    public class HistoriaClinica
+    {
+        public int Id { get; set; } 
+
+        public int PacienteId { get; set; } 
+        public Paciente Paciente { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+        public string ObservacionesGenerales { get; set; }
+
+        public List<AntecedenteMedico> AntecedentesMedicos { get; set; }
+    }
+
+}
