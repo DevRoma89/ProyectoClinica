4a409dc695d50c985e9f8c6894cec7839ceab71b
DevRoma89 <161607751+DevRoma89@users.noreply.github.com>
2026-03-06 15:14:55 -0300

    Update Receta.cs

--- Server/ProyectoClinica.Shared/Entidades/Receta.cs
+++ Server/ProyectoClinica.Shared/Entidades/Receta.cs
@@ -9,10 +9,11 @@ namespace ProyectoClinica.Shared.Entidades
     public class Receta
     {
         public int Id { get; set; }                   // Clave primaria
-        public int MedicoId { get; set; }             // FK hacia Medico
-        public Medico Medico { get; set; }            // Relación con Medico
-        public DateTime FechaCreacion { get; set; }   // Fecha de creación de la receta
-        public string Observaciones { get; set; }     // Notas generales del médico
+        public int ConsultaId {get; set;}
+        public Consulta Consulta {get; set;}
+        
+        public DateTime FechaEmision { get; set; }   // Fecha de creación de la receta
+        public string IndicacionesGenerales { get; set; }     // Notas generales del médico
 
         // Lista de detalles de la receta
         public List<DetalleReceta> Detalles { get; set; } = new List<DetalleReceta>();
