d15144537ebceb810a8fea6d01afad5f0617a5d0
Augusto Ojeda Notebook <manuojedauniversidad@gmail.com>
2026-03-07 19:45:23 -0300

    feat(autentificacion) creacion de sistema de autentificacion con JWT y usuarios

---
+++ Server/ProyectoClinica.Shared/DTOs/UsuarioDTOs/CredencialesUsuarioDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoClinica.Shared.DTOs.UsuarioDTOs
+{
+    public class CredencialesUsuarioDTO
+    {
+
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+        [Required]
+        public string?  Password { get; set; }       
+        [Required]
+        public string UserName { get; set; }
+
+    }
+}
---
+++ Server/ProyectoClinica.Shared/DTOs/UsuarioDTOs/RespuestaAutenticacionDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoClinica.Shared.DTOs.UsuarioDTOs
+{
+    public class RespuestaAutenticacionDTO
+    {
+
+        public required string Token { get; set; }
+        public DateTime Expiracion { get; set; }
+
+    }
+}
